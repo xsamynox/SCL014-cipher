10cfc903619be81f3d88d41f14b3b6ffcbb41f7a
Natalia Goicoechea <na.goicoechea@gmail.com>
2020-06-12 21:06:05 -0400

    add index.html index.js package-lock.json

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+//Botón entrar
+document.getElementById('button-get-in').addEventListener('click', showSecretName);
+//Muestra nombre secreto
+function showSecretName() {
+    let userName = document.getElementById('secret-name').value;
+    document.getElementById('user-name').innerHTML = userName;
+}
